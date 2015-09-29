1e8d58341f5b4fce984386f3507afb5f70012fff
igorkobilous <ihorbilous@yandex.ru>
2015-09-29 20:03:35 +0300

    home work status message

--- students/views/students.py
+++ students/views/students.py
@@ -111,7 +111,7 @@ def students_add(request):
 				student.save()
 
 				#redirect user to students list
-				return HttpResponseRedirect(u'%s?status_message=Студента успішно додано!' % reverse('home'))
+				return HttpResponseRedirect(u'%s?status_message=Студента %s %s успішно додано!' % (reverse('home'),student.first_name, student.last_name))
 
 			else:
 				#render form with errors and previous user input

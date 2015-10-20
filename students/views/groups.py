f044b1165c86635a96a232d9f91f301a94d76c0d
igorkobilous <ihorbilous@yandex.ru>
2015-10-20 17:44:56 +0300

    edit group

--- students/views/groups.py
+++ students/views/groups.py
@@ -53,6 +53,7 @@ def groups_add(request):
             else:
                 data['title'] = title
 
+
             leader = request.POST.get('leader', '').strip()
             if leader:
                 leader_id = Group.objects.values_list('leader_id')
@@ -77,7 +78,7 @@ def groups_add(request):
                 group = Group(**data)
                 #save it to database
                 group.save()
-
+                messages.warning(request, u"Групу %s успіхно додано!" % group.title)
                 #redirect user to groups list
                 return HttpResponseRedirect(reverse('groups'))
             else:
@@ -94,8 +95,66 @@ def groups_add(request):
         return render(request, 'students/groups_add.html',
            {'students': Student.objects.all().order_by('last_name')})
 
-def groups_edit(request, gid):
-    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+def groups_edit(request, pk):
+    if request.method == "POST":
+        #was form add button clicke?
+        if request.POST.get('add_button') is not None:
+            errors = {}
+            data = {'notes': request.POST.get('notes')}
+
+            title = request.POST.get('title', '').strip()
+            if not title:
+                errors['title'] = u"Назва групи є обов'язкова"
+            else:
+                data['title'] = title
+
+
+            leader = request.POST.get('leader', '').strip()
+            if leader:
+                if leader:
+                    leader_id = Group.objects.values_list('leader_id')
+                    list_leader_id = []
+                    student_id = ''
+
+                    for i in leader_id:
+                        i = str(i)
+                        i = i[1:(len(i)-3)]
+                        list_leader_id.append(i)
+                    for i in reversed(leader):
+                        if i.isdigit(): student_id += i
+                        else: break
+                    student_id = student_id[::-1]
+
+                    if student_id in list_leader_id:
+                        errors['leader'] = u"Цей студент є старостою в іншій групі"
+                    else:
+                        data['leader'] = Student.objects.get(pk=leader)
+
+            if not errors:
+                group = Group(**data)
+                #save it to database
+                group.save()
+                messages.warning(request, u"Групу %s успіхно редаговано!" % group.title)
+                #redirect user to groups list
+                return HttpResponseRedirect(reverse('groups'))
+            else:
+                #render form with errors and previous user input
+                messages.warning(request, u"Будь ласка виправте наступні помилки!")
+                return render(request, 'students/groups_edit.html',
+                    {'pk': pk,
+                    'students': Student.objects.all().order_by('last_name'),
+                    'group': Group.objects.get(pk=pk),
+                    'leader': Group.objects.get(pk=pk).leader,
+                    'errors': errors})
+        elif request.POST.get('cancel_button') is not None:
+            messages.warning(request, u"Редагування групи скасовано!")
+            return HttpResponseRedirect(reverse('groups'))
+    else:
+        return render(request, 'students/groups_edit.html',
+                {'pk': pk,
+                'students': Student.objects.all().order_by('last_name'),
+                'group': Group.objects.get(pk=pk),
+                'leader': Group.objects.get(pk=pk).leader})
 
 def groups_delete(request, gid):
     return HttpResponse('<h1>Delete Group %s</h1>' % gid)
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from students.views.students import StudentUpdateView, StudentDeleteView
 from students.views.contact_admin import ContactFormView
+from students.models import Group
 
 from .settings import MEDIA_ROOT, DEBUG
 
@@ -31,13 +32,13 @@ urlpatterns = patterns('',
     url(r'^students/(?P<pk>\d+)/delete/$',
         StudentDeleteView.as_view(),
         name='students_delete'),
-    
+
 
     # Groups urls
     url(r'^groups/$', 'students.views.groups.groups_list', name='groups'),
     url(r'^groups/add/$', 'students.views.groups.groups_add',
         name='groups_add'),
-    url(r'^groups/(?P<gid>\d+)/edit/$',
+    url(r'^groups/(?P<pk>\d+)/edit/$',
         'students.views.groups.groups_edit',
         name='groups_edit'),
     url(r'^groups/(?P<gid>\d+)/delete/$',
